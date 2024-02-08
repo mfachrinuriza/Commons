a88439c91c132b116aafa5350b2a8c6dcc64a623
Muhammad Fachri Nuriza <fachri.nuriza@sg-dsa.com>
2024-02-08 20:14:25 +0700

    Initial Commit

---
+++ Commons/Commons.h
@@ -0,0 +1,18 @@
+//
+//  Commons.h
+//  Commons
+//
+//  Created by Muhammad Fachri Nuriza on 08/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Commons.
+FOUNDATION_EXPORT double CommonsVersionNumber;
+
+//! Project version string for Commons.
+FOUNDATION_EXPORT const unsigned char CommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commons/PublicHeader.h>
+
+
